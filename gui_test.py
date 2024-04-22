4a93b397944759437d3a6ac60179ace02ce6557f
gdederichs <gregor.dederichs@epfl.ch>
2024-04-22 19:54:37 +0200

    adding ramps and blind mode to GUI

--- gui_test.py
+++ gui_test.py
@@ -4,6 +4,7 @@ from PyQt6.QtWidgets import (QApplication,
                             QLabel,
                             QWidget,
                             QPushButton,
+                            QCheckBox,
                             QGridLayout)
 
 import pyqtgraph as pg
@@ -78,15 +79,15 @@ class MainWindow(QWidget):
         self.total_iTBS_time_edit = QLineEdit(str(util.total_iTBS_time))
         self.layout.addWidget(self.total_iTBS_time_edit,0,1)
 
-        # time of stimulation in one cycle
-        self.layout.addWidget(QLabel('Cycle Stimulation Duration (seconds):'), 1, 0)
-        self.cycle_stim_time_edit = QLineEdit(str(util.cycle_stim_time))
-        self.layout.addWidget(self.cycle_stim_time_edit,1,1)
+        # time of stimulation in one cycle (train)
+        self.layout.addWidget(QLabel('Train Stimulation Duration (seconds):'), 1, 0)
+        self.train_stim_time_edit = QLineEdit(str(util.train_stim_time))
+        self.layout.addWidget(self.train_stim_time_edit,1,1)
 
-        # time of break in on cycle 
-        self.layout.addWidget(QLabel('Cycle Break Duration (seconds):'), 2, 0)
-        self.cycle_break_time_edit = QLineEdit(str(util.cycle_break_time))
-        self.layout.addWidget(self.cycle_break_time_edit,2,1)
+        # time of break in on cycle (train)
+        self.layout.addWidget(QLabel('Train Break Duration (seconds):'), 2, 0)
+        self.train_break_time_edit = QLineEdit(str(util.train_break_time))
+        self.layout.addWidget(self.train_break_time_edit,2,1)
 
         # pulse frequency
         self.layout.addWidget(QLabel('Pulse Frequency (Hz):'), 3, 0)
@@ -94,25 +95,24 @@ class MainWindow(QWidget):
         self.layout.addWidget(self.freq_of_pulse_edit,3,1)
 
         # cycle frequency
-        self.layout.addWidget(QLabel('Cycle Frequency (Hz):'), 4, 0)
-        self.cycle_freq_edit = QLineEdit(str(util.cycle_freq))
-        self.layout.addWidget(self.cycle_freq_edit,4,1)
+        self.layout.addWidget(QLabel('Burst Frequency (Hz):'), 4, 0)
+        self.burst_freq_edit = QLineEdit(str(util.burst_freq))
+        self.layout.addWidget(self.burst_freq_edit,4,1)
 
         # carrier frequency
         self.layout.addWidget(QLabel('Carrier Frequency (Hz):'), 5, 0)
         self.carrier_f_edit = QLineEdit(str(util.carrier_f))
         self.layout.addWidget(self.carrier_f_edit,5,1)
 
+        # sum of amplitudes
+        self.layout.addWidget(QLabel('Amplitude Sum (mA):'), 6, 0)
+        self.A_sum_edit = QLineEdit(str(util.A_sum))
+        self.layout.addWidget(self.A_sum_edit,6,1)
 
-        # amplitude of signal 1
-        self.layout.addWidget(QLabel('Amplitude 1 ():'), 6, 0)
-        self.A1_edit = QLineEdit(str(util.ampli1))
-        self.layout.addWidget(self.A1_edit,6,1)
-
-        # amplitude of signal 2
-        self.layout.addWidget(QLabel('Amplitude 2 ():'), 7, 0)
-        self.A2_edit = QLineEdit(str(util.ampli2))
-        self.layout.addWidget(self.A2_edit,7,1)
+        # ratio of amplitudes
+        self.layout.addWidget(QLabel('Amplitude Ratio (A1/A2):'), 7, 0)
+        self.A_ratio_edit = QLineEdit(str(util.A_ratio))
+        self.layout.addWidget(self.A_ratio_edit,7,1)
 
 
         # BUTTON FIELDS
@@ -147,11 +147,48 @@ class MainWindow(QWidget):
         self.btn_stop.clicked.connect(self.request_stop)
         self.layout.addWidget(self.btn_stop,11,1)
 
+        # WIDGETS FOR EXPERIMENT MODE (BLIND)
+        # experiment mode toggle
+        self.exp_mode = QCheckBox('Experiment Mode')
+        self.layout.addWidget(self.exp_mode,12,1)
+        self.exp_mode.stateChanged.connect(self.check_exp_mode)
+
+        # subject 
+        self.label_subject = QLabel('Subject:')
+        self.layout.addWidget(self.label_subject, 0, 0)
+        self.subject_edit = QLineEdit("")
+        self.subject_edit.setPlaceholderText("Subject ID")
+        self.layout.addWidget(self.subject_edit,0,1)
+        self.label_subject.hide()
+        self.subject_edit.hide()
+
+        # session
+        self.label_session = QLabel('Session:')
+        self.layout.addWidget(self.label_session, 1, 0)
+        self.session_edit = QLineEdit("")
+        self.session_edit.setPlaceholderText("Session ID")
+        self.layout.addWidget(self.session_edit,1,1)
+        self.label_session.hide()
+        self.session_edit.hide()
+
+        # file name storing data for stimulation
+        self.label_file = QLabel('File storing data for stimulation:')
+        self.layout.addWidget(self.label_file, 2, 0)
+        self.file_edit = QLineEdit("")
+        self.file_edit.setPlaceholderText("your\\file\\path\\here\\for\\stim\\data.xlxs")
+        self.layout.addWidget(self.file_edit,2,1)
+        self.label_file.hide()
+        self.file_edit.hide()
+
+
+
 
         # GRAPH FIELDS
         self.dt = [0,1]
         self.iTBS_signals = np.zeros((2,2))
         self.plot_waveform = pg.PlotWidget()
+        self.plot_waveform.getAxis("bottom").setLabel("Time", units="s")
+        self.plot_waveform.getAxis("left").setLabel("Amplitude", units="mA")
         self.plot_waveform.plot(self.dt, self.iTBS_signals[0]+self.iTBS_signals[1])
         self.layout.addWidget(self.plot_waveform,0,2,14,1)
 
@@ -167,42 +204,51 @@ class MainWindow(QWidget):
 
         self.setLayout(self.layout)
         self.show()
+        if util.default_mode=="Experiment":
+            self.exp_mode.setCheckState(Qt.CheckState.Checked)
 
     # FUNCTIONS
     def create_signals(self):
         self.total_iTBS_time = int(self.total_iTBS_time_edit.text())
-        self.cycle_stim_time = int(self.cycle_stim_time_edit.text())
-        self.cycle_break_time = int(self.cycle_break_time_edit.text())
+        self.train_stim_time = int(self.train_stim_time_edit.text())
+        self.train_break_time = int(self.train_break_time_edit.text())
         self.freq_of_pulse = int(self.freq_of_pulse_edit.text())
-        self.cycle_freq = int(self.cycle_freq_edit.text())
+        self.burst_freq = int(self.burst_freq_edit.text())
         self.carrier_f = int(self.carrier_f_edit.text())
-        self.A1 = float(self.A1_edit.text())
-        self.A2 = float(self.A2_edit.text())
+        self.A_sum = float(self.A_sum_edit.text())
+        self.A_ratio = float(self.A_ratio_edit.text())
+        self.A1 = self.A_sum/(1+self.A_ratio)
+        self.A2 = self.A_ratio*self.A_sum/(1+self.A_ratio)
+
+
         self.dt, self.iTBS_signals = util.iTBS(self.total_iTBS_time,
-                                     self.cycle_stim_time,
-                                     self.cycle_break_time,
+                                     self.train_stim_time,
+                                     self.train_break_time,
                                      self.freq_of_pulse,
-                                     self.cycle_freq,
+                                     self.burst_freq,
                                      self.carrier_f,
                                      self.A1, self.A2)
         
         
     def graph_waveform(self):
-        self.plot_waveform = pg.PlotWidget()
-        self.plot_waveform.plotItem.setMouseEnabled(y=False) # Only allow zoom in X-axis
-        self.plot_waveform.plot(self.dt, self.iTBS_signals[0]+self.iTBS_signals[1])
-        self.layout.addWidget(self.plot_waveform,0,2,14,1)
+        if self.exp_mode.checkState() == Qt.CheckState.Unchecked:
+            self.plot_waveform = pg.PlotWidget()
+            self.plot_waveform.plotItem.setMouseEnabled(y=False) # Only allow zoom in X-axis
+            self.plot_waveform.plot(self.dt,self.iTBS_signals[0]+self.iTBS_signals[1],)
+            self.plot_waveform.getAxis("bottom").setLabel("Time", units="s")
+            self.plot_waveform.getAxis("left").setLabel("Amplitude", units="mA")
+            self.layout.addWidget(self.plot_waveform,0,2,14,1)
 
         
     def reset_defaults(self):
         self.total_iTBS_time_edit.setText(str(util.total_iTBS_time))
-        self.cycle_stim_time_edit.setText(str(util.cycle_stim_time))
-        self.cycle_break_time_edit.setText(str(util.cycle_break_time))
+        self.train_stim_time_edit.setText(str(util.train_stim_time))
+        self.train_break_time_edit.setText(str(util.train_break_time))
         self.freq_of_pulse_edit.setText(str(util.freq_of_pulse))
-        self.cycle_freq_edit.setText(str(util.cycle_freq))
+        self.burst_freq_edit.setText(str(util.burst_freq))
         self.carrier_f_edit.setText(str(util.carrier_f))
-        self.A1_edit.setText(str(util.ampli1))
-        self.A2_edit.setText(str(util.ampli2))
+        self.A_sum_edit.setText(str(util.A_sum))
+        self.A_ratio_edit.setText(str(util.A_ratio))
 
 
     def run_stimulation(self):
@@ -235,6 +281,30 @@ class MainWindow(QWidget):
         self.stop_request = True
 
 
+    def check_exp_mode(self):
+        if self.exp_mode.checkState() == Qt.CheckState.Checked:
+            for widget in self.findChildren(QWidget):
+                if (widget != self.exp_mode and
+                    widget != self.btn_run_stimulation and
+                    widget != self.btn_create_signals and
+                    widget != self.btn_update and
+                    widget != self.run_status and
+                    widget != self.btn_stop
+                    ): 
+                    widget.setVisible(not widget.isVisible())
+
+        if self.exp_mode.checkState() == Qt.CheckState.Unchecked:
+            for widget in self.findChildren(QWidget):
+                if (widget != self.exp_mode and
+                    widget != self.btn_run_stimulation and
+                    widget != self.btn_create_signals and
+                    widget != self.btn_update and
+                    widget != self.run_status and
+                    widget != self.btn_stop
+                    ): 
+                    widget.setVisible(not widget.isVisible())
+
+
 
 
 # RUN APPLICATION
@@ -247,4 +317,25 @@ sys.exit(app.exec())
 CURRENT ISSUES:
 -with stop: works in both multi_channel_test.py and GUI, but ONLY IF stim is in break (outputting zeros).
             else: spikes as usual
+-update works at any time --> for stop, update to null function, for 2/3 seconds and then call stop
+-next:  -put correct value fields in GUI/clean GUI, with more headers, information etc/compare with MATLAB GUI and build towards that
+        -make GUI nicer looking
+        -add drop down menu to choose waveform?
+        -with trigger?
+
+-next:  DONE - 1) Rename cycle durations to train durations
+        DONE - 2) Rename cycle freq to burst freq
+        DONE - 3) Amplitudes should be defined by sum and ratio
+        DONE - 4) Default amplitudes are 2 (sum is 4)
+        DONE - 5) Add axis titles to graphing
+        DONE - 6) Add ramp ups and ramp downs (No shift in carrier freqs, Amplitude gradually increase)
+        DONE - 6b) Between trains: no shift in high stim, not simply 0
+        6c) Add ramp params to GUI, and add lines on graph after rampup and befor rampdown (for visu)
+        7) Add stop function with better functionality (update to ramp down)
+        8) Add menu to choose stimulation type
+        9) Save file with all parameters used after stimulation
+        STARTED - 10) Add experiment mode
+                ->need to add reading from excel data if exp mode.
+                    ->which data are stored in excel file?
+        11) Trigger
 """
--- util.py
+++ util.py
@@ -4,23 +4,29 @@ from nidaqmx.constants import AcquisitionType
 from nidaqmx.constants import WAIT_INFINITELY as inf
 
 '''
-Parameters for the main program
+Deafult parameters for the main user interface
 '''
+#for GUI
+default_mode = "Experiment" #in experiment mode, GUI is blind (should be "Experiment" or "Settings")
+
 #for DAQ
 device = "Dev4"
 
 #for iTBS  ---  time in seconds, frequency in Hz
 total_iTBS_time = 20 #time of entire signal; in s
-cycle_stim_time = 2 #time of a single stimulation phase
-cycle_break_time = 8 #break time after the stimulation phase
+train_stim_time = 2 #time of a single stimulation phase
+train_break_time = 8 #break time after the stimulation phase
 
 carrier_f = 2000 #frequency of individual signals
 sampling_f = 100000 #matlab has dt = 0.01ms
 freq_of_pulse = 100 #frequency of envelope
-cycle_freq = 5 #within a stimulation phase, frequency of theta-bursts
-ampli1 = 0.5 #amplitude of signal 1
-ampli2 = 0.5 #amplitude of signal 2
-
+burst_freq = 5 #within a stimulation phase, frequency of theta-bursts
+A_sum = 4 #sum of amplitudes
+A_ratio = 1 #ratio of amplitudes
+ampli1 = A_sum/(1+A_ratio) #amplitude of signal 1
+ampli2 = A_ratio*A_sum/(1+A_ratio) #amplitude of signal 2
+ramp_up_time = 5 #in s; high freq stim with no shift (no pulse), ramping aplitude
+ramp_down_time = 5 #in s
 
 #general
 ampl = 2
@@ -36,47 +42,67 @@ should_print  = True
 
 
 def iTBS(total_time = total_iTBS_time,
-         stim_time = cycle_stim_time,
-         break_time = cycle_break_time,
+         stim_time = train_stim_time,
+         break_time = train_break_time,
          pulse_f = freq_of_pulse,
-         cycle_f = cycle_freq,
+         burst_f = burst_freq,
          carrier_f = carrier_f,
          A1 = ampli1,
          A2 = ampli2,
+         ramp_up_time = ramp_up_time,
+         ramp_down_time = ramp_down_time,
          plot = False):
 
     no_cycles = np.floor(total_time/(stim_time+break_time)-0.001)
-    dt = np.linspace(0,total_time,int(sampling_f*total_time))
+    dt = np.linspace(0,total_time+ramp_up_time+ramp_down_time, int(sampling_f*(total_time+ramp_up_time+ramp_down_time)))
+    break_dt = np.linspace(0, break_time, int(sampling_f*break_time))
 
     #creating signals
-    signals = createTI(high_f=carrier_f,
+    # ========== RAMP UP ==========
+    signals = ramp(direction="up", carrier_f=carrier_f, ramp_up_time=ramp_up_time, A1_max=A1, A2_max=A2)
+
+    # ======== MAIN SIGNAL ========
+    first_sig = createTI(high_f=carrier_f,
                        pulse_f=pulse_f,
-                       cycle_f=cycle_f,
+                       burst_f=burst_f,
                        duration=stim_time,
                        A1=A1,
                        A2=A2)
-    signals = np.concatenate((signals, np.zeros((2,sampling_f*break_time))), axis=1)
+    signals = np.concatenate((signals, first_sig), axis=1)
+
+    I1b = A1*np.cos(2*np.pi*carrier_f*break_dt)
+    I2b = A2*np.cos(2*np.pi*carrier_f*break_dt+np.pi)
+    signals = np.concatenate((signals, np.vstack((I1b,I2b))), axis=1)
     
     for i in np.arange(no_cycles):
         new = createTI(high_f=carrier_f,
                        pulse_f=pulse_f,
-                       cycle_f=cycle_f,
+                       burst_f=burst_f,
                        duration=stim_time,
                        A1=A1,
                        A2=A2)
         signals = np.concatenate((signals, new), axis=1)
         
         if i!=no_cycles-1:
-            signals = np.concatenate((signals, np.zeros((2,sampling_f*break_time))), axis=1)
+            I1b = A1*np.cos(2*np.pi*carrier_f*break_dt)
+            I2b = A2*np.cos(2*np.pi*carrier_f*break_dt+np.pi)
+            signals = np.concatenate((signals, np.vstack((I1b,I2b))), axis=1)
         else: #last stim needs special care to correctly fit time
             dt_dim = np.size(dt)
             sig_dim = np.shape(signals)[1]
             # fill in remaining time
             if dt_dim-sig_dim>0:
-                signals = np.concatenate((signals, np.zeros((2,dt_dim-sig_dim))), axis=1)
-            # or increase time to allow end of signal
-            else:
-                dt = np.linspace(0, (sig_dim - 1) * (dt[1]-dt[0]), sig_dim)
+                I1b = A1*np.cos(2*np.pi*carrier_f*break_dt)
+                I2b = A2*np.cos(2*np.pi*carrier_f*break_dt+np.pi)
+                signals = np.concatenate((signals, np.vstack((I1b,I2b))), axis=1)
+                
+
+    # ========= RAMP DOWN =========
+    down = ramp(direction="down", carrier_f=carrier_f, ramp_up_time=ramp_up_time, A1_max=A1, A2_max=A2)
+    signals = np.concatenate((signals, down), axis=1)
+    # add 100 zeros to offset spiking - update dt accordingly 
+    signals = np.concatenate((signals, np.zeros((2,100))), axis=1)
+    dt = np.concatenate((dt, dt[-1] + np.arange(0, 100) * dt[1]-dt[0]))
     
     if plot:
         I1 = signals[0]
@@ -87,19 +113,19 @@ def iTBS(total_time = total_iTBS_time,
         #plt.plot(dt,I2)
         plt.show()
 
-    return dt, signals
+    return dt, signals #need to update dt to get rampups and rampdowns in signal
 
 
 
 def createTI(high_f = carrier_f,
              pulse_f = freq_of_pulse,
-             cycle_f = cycle_freq,
-             duration = cycle_stim_time,
+             burst_f = burst_freq,
+             duration = train_stim_time,
              A1 = ampli1,
              A2 = ampli2,
              plot = False):
     
-    cycle_t = 1/cycle_f #related to cycle frequency
+    cycle_t = 1/burst_f #related to cycle frequency
     pulse_t = 3/pulse_f 
     no_pulses = int(duration/cycle_t)
     dt = np.linspace(0,duration,int(sampling_f*duration))
@@ -137,6 +163,27 @@ def createTI(high_f = carrier_f,
 
 
 
+def ramp(direction = "up",
+        carrier_f = carrier_f,
+        ramp_up_time = ramp_up_time,
+        A1_max=ampli1,
+        A2_max=ampli2):
+
+        dt = np.linspace(0,ramp_up_time,int(sampling_f*ramp_up_time))
+
+        ramp1 = np.linspace(0,A1_max,int(sampling_f*ramp_up_time))
+        ramp2 = np.linspace(0,A2_max,int(sampling_f*ramp_up_time))
+        
+        if direction=="down":
+            ramp1 = ramp1[::-1]
+            ramp2 = ramp2[::-1]
+        elif direction != "up":
+            raise ValueError("parameter 'direction' should be either 'up' or 'down' (case sensitive)")
+
+        I1 = ramp1*np.cos(2*np.pi*carrier_f*dt)
+        I2 = ramp2*np.cos(2*np.pi*carrier_f*dt+np.pi)
+
+        return np.vstack((I1,I2))
 
 
 def subdivide(dt, signal, chunk_time):
