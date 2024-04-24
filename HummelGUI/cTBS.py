17d95be5fa730d185711547a3deff6d291f30ad2
gdederichs <gregor.dederichs@epfl.ch>
2024-04-24 15:37:18 +0200

    documentation, continued

--- HummelGUI/cTBS.py
+++ HummelGUI/cTBS.py
@@ -10,7 +10,45 @@ def cTBS(total_time = util.total_iTBS_time,
          ramp_up_time = util.ramp_up_time,
          ramp_down_time = util.ramp_down_time,
          rampup = True):
+    ''' 
+    Description
+    -----------
+    Create an continuous Theta Burst Stimulation (cTBS) signal
     
+    Parameters
+    ----------
+    total_time : int
+        the time in seconds that the stimulation lasts (excluding ramp-up and ramp-down)
+        
+    pulse_f : int
+        the frequency in Hz of the envelope (pulse)
+
+    burst_f : int
+        the frequency in Hz at which theta-bursts (3 pulses) occur 
+
+    carrier_f : int
+        the frequency in Hz of the individual signals (ie. high frequency)
+
+    A1 : float
+        the amplitude in mA of signal 1
+
+    A2 : float
+        the amplitude in mA of signal 2
+
+    ramp_up_time : int
+        the time in seconds of amplitude increase of high frequency signals, before stimulation
+
+    ramp_down_time : int
+        the time in seconds of amplitude decrease of high frequency signals, after stimulation
+
+    rampup : bool
+        True if rampup is to be included (eg. False for updating signal during stimulation)
+
+    Returns
+    -------
+    tuple[np.array, np.array]
+        the time points and the signals (eg. signal 1 is signals[0])
+    '''
     dt = np.linspace(0,total_time+ramp_down_time, int(util.sampling_f*(total_time+ramp_down_time)))
     # ========== RAMP UP ==========
     if rampup:
